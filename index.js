9e3912db3793055f450639246af26a1bba2f8013
Asif-Ul-Karim-Nabir <karimnabir17@gmail.com>
2021-10-29 11:07:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+//  MidleWare
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
