62b302e27279811fd6d3489eb27c6b7e3f7974f2
Asif-Ul-Karim-Nabir <karimnabir17@gmail.com>
2021-10-31 16:34:53 +0600

    second-commit

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //  MidleWare
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jdcg5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run () {
+  try{
+    await client.connect()
+
+    const database = client.db("secure_travel")
+    const planCollection = database.collection('plans')
+    
+    // GET API
+    // app.get('/plans', async(req,res)=>{
+    //   const cursor = planCollection.find({})
+    //   const plans = await cursor.toArray()
+    //   res.json(plans)
+    //   console.log(result);
+    // })
+  }
+  finally{
+    // await client.close()
+  }
+}
+ 
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
